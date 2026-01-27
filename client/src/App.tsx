8ef0282ea9fda5badf6454a9049ffcf7b4741edc
Mingu Kim <mingufkim@gmail.com>
2026-01-27 19:54:45 +0900

    🎉 Oowee client setup

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import {Button} from "@/components/ui/button"
+
+function App() {
+
+  return (
+    <>
+      <div className="flex h-screen w-full flex-col items-center justify-center gap-4 bg-slate-50">
+        <h1 className="text-8xl font-black tracking-tight">
+          Oowee.
+        </h1>
+        <Button>shadcn/ui</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
